112e7b2fefed38a4d2d4652ce57a03e3249fac7d
bien-123 <bienle160720@gmail.com>
2023-11-18 17:04:59 +0700

    Feat search services user

--- src/assets/logo/index.jsx
+++ src/assets/logo/index.jsx
@@ -2,7 +2,8 @@ import LogoMain from './LogoMain.svg';
 import LogoMBBank from './LogoMBBank.svg';
 import LogoGlobalAdmin from './logoGlobalAdmin.svg';
 import DangKy from './Dang-ky-copy-3.png';
+import LogoEmpty from './Vector.svg';
 
-const logo = { LogoMain, LogoMBBank, LogoGlobalAdmin, DangKy };
+const logo = { LogoMain, LogoMBBank, LogoGlobalAdmin, DangKy, LogoEmpty };
 
 export default logo;
