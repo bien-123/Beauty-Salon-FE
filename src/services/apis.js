531cdccc4e031f3acc014de1686e58a826a1a5fd
Bien Le <bienle160720@gmail.com>
2024-03-31 17:12:41 +0700

    Call API BE deploy

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT_PUBLIC = 'http://localhost:5000';
+export const API_ENDPOINT_PUBLIC = 'https://beauty-salon-be.onrender.com';
 
 export const API_STAFF_PAGE = `${API_ENDPOINT_PUBLIC}/staff`;
 
